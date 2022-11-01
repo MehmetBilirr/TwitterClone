0c2aa7b3805c8347f3afb833523e4a080251af59
MehmetBilirr <mehmetbilir@aol.com>
2022-11-02 01:01:13 +0300

    start for UIKIT

--- TwitterClone/MainTabBarViewController.swift
+++ TwitterClone/MainTabBarViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-
+    
 
 }
 
