7576edbaba2ccb1ea54ea065a7a1a8b386f643de
MehmetBilirr <mehmetbilir@aol.com>
2022-11-02 00:29:40 +0300

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Mehmet Bilir on 2.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
