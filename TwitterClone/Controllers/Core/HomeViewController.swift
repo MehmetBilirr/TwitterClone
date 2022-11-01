b40672adaaab81d0d61afeb0657ec49ff8ab194a
MehmetBilirr <mehmetbilir@aol.com>
2022-11-02 01:20:13 +0300

    HomeViewController NavBar items added

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -12,10 +12,28 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       configureNavigationBar()
     }
     
 
-    
+    private func configureNavigationBar(){
+        let size:CGFloat = 36
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.image = UIImage(named: "twitterLogo")
+        navigationItem.titleView = logoImageView
+        
+        
+        let profileImage = UIImage(systemName: "person")
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: profileImage, style: .plain, target: self, action: #selector(didTapProfile(_:)))
+    }
 
+}
+
+extension HomeViewController {
+    
+    @objc func didTapProfile(_ sender:UIButton) {
+        print("profile button tapped")
+    }
 }
