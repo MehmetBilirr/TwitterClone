991cff8b453e7e918ac66664a43cef867a700962
MehmetBilirr <mehmetbilir@aol.com>
2022-11-02 18:11:39 +0300

    tweetTableViewCell designed

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let timeLineTableView = UITableView()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
        configureNavigationBar()
+        configureTableView()
+        
+        view.backgroundColor = .systemBackground
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        timeLineTableView.frame = view.bounds
+    }
+    
+    
+    private func configureTableView(){
+        view.addSubview(timeLineTableView)
+        timeLineTableView.delegate = self
+        timeLineTableView.dataSource = self
+        
+        timeLineTableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
+        
+        
+    }
 
     private func configureNavigationBar(){
         let size:CGFloat = 36
@@ -23,17 +42,54 @@ class HomeViewController: UIViewController {
         logoImageView.image = UIImage(named: "twitterLogo")
         navigationItem.titleView = logoImageView
         
+    
         
-        let profileImage = UIImage(systemName: "person")
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: profileImage, style: .plain, target: self, action: #selector(didTapProfile(_:)))
+        
+        let userImageView = UIImageView()
+        userImageView.image = UIImage(named: "UserImage")
+        userImageView.layer.cornerRadius = 20
+        userImageView.clipsToBounds = true
+        userImageView.contentMode = .scaleAspectFit
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapProfile))
+        userImageView.addGestureRecognizer(tapGesture)
+        userImageView.isUserInteractionEnabled = true
+        userImageView.frame.size = CGSize(width: 36, height: 36)
+        
+        let backButtonItem = UIBarButtonItem()
+        backButtonItem.customView = UIView(frame: CGRect(x:0, y:0, width: 36, height: 36))
+        backButtonItem.customView?.addSubview(userImageView)
+        navigationItem.leftBarButtonItem = backButtonItem
+        
+        
+        let sparklesImage = UIImage(systemName: "sparkles")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: sparklesImage, style: .plain, target: self, action: nil)
+        
     }
 
 }
 
+
+// Extension - Button
 extension HomeViewController {
     
-    @objc func didTapProfile(_ sender:UIButton) {
+    @objc func didTapProfile() {
         print("profile button tapped")
     }
+}
+
+//Extension - TableView
+extension HomeViewController:UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as! TweetTableViewCell
+        return cell
+    }
+    
+    
+    
 }
---
+++ TwitterClone/Views/TweetTableViewCell.swift
@@ -0,0 +1,150 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterClone
+//
+//  Created by Mehmet Bilir on 2.11.2022.
+//
+
+import UIKit
+import SnapKit
+
+class TweetTableViewCell: UITableViewCell {
+    static let identifier = "TweetTableViewCell"
+    private let userImageView = UIImageView()
+    private let userStackView = UIStackView()
+    private let nameLbl = UILabel()
+    private let userNameLbl = UILabel()
+    private let tweetLbl = UILabel()
+    private let buttonStackView = UIStackView()
+    private let replyButton = UIButton()
+    private let retweetButton = UIButton()
+    private let likeButton = UIButton()
+    private let shareButton = UIButton()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        stylee()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    private func stylee(){
+        userImageView.translatesAutoresizingMaskIntoConstraints = false
+        userImageView.contentMode = .scaleAspectFit
+        userImageView.clipsToBounds = true
+        userImageView.layer.cornerRadius = 25
+        userImageView.layer.masksToBounds = true
+        
+        userImageView.image = UIImage(named: "UserImage")
+        
+        userStackView.translatesAutoresizingMaskIntoConstraints = false
+        userStackView.axis = .horizontal
+        userStackView.spacing = 5
+        
+        userNameLbl.translatesAutoresizingMaskIntoConstraints = false
+        userNameLbl.textAlignment = .center
+        userNameLbl.font = .systemFont(ofSize: 14, weight: .regular)
+        userNameLbl.textColor = .gray
+        userNameLbl.text = "@mbilir9"
+        
+        nameLbl.translatesAutoresizingMaskIntoConstraints = false
+        nameLbl.textAlignment = .center
+        nameLbl.font = .systemFont(ofSize: 12, weight: .bold)
+        nameLbl.textColor = .white
+        nameLbl.text = "Mehmet Bilir"
+        
+        tweetLbl.translatesAutoresizingMaskIntoConstraints = false
+        tweetLbl.textAlignment = .left
+        tweetLbl.font = .systemFont(ofSize: 14, weight: .regular)
+        tweetLbl.textColor = .white
+        tweetLbl.numberOfLines = 0
+        tweetLbl.lineBreakMode = .byWordWrapping
+        tweetLbl.text = "Hello There! I am Mehmet Bilir.I'am  currently learning Swift and looking for job as iOS Developer. "
+        
+        
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        buttonStackView.axis = .horizontal
+        buttonStackView.spacing = 70
+        
+        
+        replyButton.translatesAutoresizingMaskIntoConstraints = false
+        replyButton.setImage(UIImage(systemName: "bubble.right"), for: .normal)
+        replyButton.tintColor = .systemGray2
+        
+        
+        retweetButton.translatesAutoresizingMaskIntoConstraints = false
+        retweetButton.setImage(UIImage(systemName: "arrow.2.squarepath"), for: .normal)
+        retweetButton.tintColor = .systemGray2
+        
+        likeButton.translatesAutoresizingMaskIntoConstraints = false
+        likeButton.setImage(UIImage(systemName: "suit.heart"), for: .normal)
+        likeButton.tintColor = .systemGray2
+        
+        
+        shareButton.translatesAutoresizingMaskIntoConstraints = false
+        shareButton.setImage(UIImage(systemName: "square.and.arrow.up"), for: .normal)
+        shareButton.tintColor = .systemGray2
+        
+    }
+    
+    private func layout(){
+        contentView.addSubview(userImageView)
+        
+        userImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(15)
+            make.top.equalToSuperview().offset(15)
+            make.height.equalTo(50)
+            make.width.equalTo(50)
+        }
+        
+        userStackView.addArrangedSubview(nameLbl)
+        userStackView.addArrangedSubview(userNameLbl)
+        contentView.addSubview(userStackView)
+        
+        userStackView.snp.makeConstraints { make in
+            make.left.equalTo(userImageView.snp.right).offset(15)
+            make.top.equalToSuperview().offset(15)
+        }
+        
+        contentView.addSubview(tweetLbl)
+        tweetLbl.snp.makeConstraints { make in
+            make.left.equalTo(userStackView.snp.left)
+            make.top.equalTo(userStackView.snp.bottom)
+            make.right.equalToSuperview().offset(-10)
+            
+        }
+        
+        
+        buttonStackView.addArrangedSubview(replyButton)
+        buttonStackView.addArrangedSubview(retweetButton)
+        buttonStackView.addArrangedSubview(likeButton)
+        buttonStackView.addArrangedSubview(shareButton)
+        contentView.addSubview(buttonStackView)
+        
+        buttonStackView.snp.makeConstraints { make in
+            make.left.equalTo(userStackView.snp.left)
+            make.top.equalTo(tweetLbl.snp.bottom).offset(15)
+            make.bottom.equalToSuperview().offset(-20)
+        }
+    }
+    
+    
+    
+}
+
+
+
+// Extension - Buttons Action
+extension TweetTableViewCell {
+    
+    
+    
+    @objc func didTapReply(){
+        
+    }
+}
